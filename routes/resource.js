70a7f1354baf78206c628b327d9f642b2d3b6fd1
anjanakrishnamurthi <anjanakr@andrew.cmu.edu>
2018-10-14 11:12:04 -0400

    Post fix save

--- routes/resource.js
+++ routes/resource.js
@@ -5,7 +5,8 @@ var path = require('path');
 var passport = require('passport');
 var cloudinary = require('cloudinary');
 var User = require('../models/user');
-var resources = require('../models/resource');
+var Resource = require('../models/resource');
+var db   = require('../secure/db');
 fs = require('fs'),
 
 router.get('/resource',function(req,res,next){
@@ -13,16 +14,43 @@ router.get('/resource',function(req,res,next){
         if(err) throw err;
         if(user)
         {
-            //var data =fs.readFileSync('./public/files/resource.pdf');
-            //res.contentType("application/pdf");
-            //res.render('resource',{title:"Steel Smiling",header:true,navbar:true,user:user,data:data});
-
-            var data =fs.readFileSync('./public/files/resource.pdf');
-            res.contentType("application/pdf");
-            res.send(data);
+            db.findAll(Resource)
+                .then(function(data){
+                    console.log(data);
+                    res.render('resource',{title:"Steel Smiling",user:req.user,header:true,navbar:true,resources:data});
+                })
+                .catch(function(err){
+                    next(err);
+                });
+            }
+            else {
+            throw(err);
         }
-
     });
+});
+
+router.post('/saveresource',function(req,res,next) {
+
+    var name = req.body.name;
+    var description = req.body.description;
+    var website = req.body. website;
+
+    async.waterfall([
+        function (callback) {
+            var newResource = new Resource();
+            newResource.name = name;
+            newResource.description = description;
+            newResource.website = website;
+            newResource.save((err, data) => {
+                if (err) res.send({msg: "Something went wrong", success: false});
+                if (data) callback(null, data);
+                console.log(data);
+            });
+        },
+    ]);
+
+
+
 });
 
 

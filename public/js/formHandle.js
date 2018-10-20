6d601ffbf9a1fa1be57bdebd8c93ff3cc55f7b7c
yourname <user@email.com>
2018-10-20 16:49:48 -0400

    Resource upload-v1.0

--- public/js/formHandle.js
+++ public/js/formHandle.js
@@ -4,6 +4,7 @@
 // declaration
 
 const signup = document.querySelector('#signup');
+const resourceupload = document.querySelector('.resourceupload');
 const resetToken  = document.querySelector('#reset');
 const signIn = document.querySelector('#signIn');
 const updateBtn = document.querySelector('.btn-save');
@@ -93,6 +94,82 @@ function register_user(e){
 
 }
 
+
+function upload_resource(e){
+    console.log("Inside upload res:")   ;
+    // stop default action
+    e.preventDefault();
+
+    const  button = this.children[this.children.length-1];
+
+    //Form Handling with ajax
+
+    $.ajax({
+
+        url      : '/saveresource',
+        method 	 : 'POST',
+        data   	 :  $(this).serialize(),
+        dataType :  'json',
+
+        beforeSend : function(http){
+            button.style.opacity = '0.7';
+            button.innerText = 'Submitting';
+            button.setAttribute("disabled", "true");
+        },
+        success    : function(response,status,http){
+            console.log("success");
+            let message = response.msg,
+                element = response.param,
+                success = response.success,
+                id      = "error",
+                icon   = "fa-times-circle",
+                flashModal = document.querySelector('#flashModal .modal-body');
+
+            if(element)  document.querySelector(`[name=${element}]`).value = "" ;
+
+            if(success) {
+                id = "success";
+                icon = "fa-check-square";
+                document.querySelector("form").reset();
+            }
+
+            flashModal.innerHTML = `<div id="${id}" class="flash">
+                        			  <i class="fa ${icon}"></i>
+                          			  <p class="lead" style="color:#a9a9a9">${message}</p>
+                      				</div>`;
+
+            //show flashModal
+            $('#flashModal').modal('show');
+
+            if(success){
+                window.addEventListener("click", function(){
+                    window.location.href = "/";
+                });
+            }
+
+
+            // set as default
+            button.style.opacity = '1';
+            button.innerText = 'Sign up';
+            button.removeAttribute("disabled");
+
+
+        },
+
+        error  : function(http,status,error){
+            console.log("Error:"+error);
+            flashModal.innerHTML = `<div id="error" class="flash">
+                        			 		 <i class="fa fa-   times-circle"></i>
+                          			  		<p class="lead" style="color:#a9a9a9">Something went wrong here.</p>
+                      					</div>`;
+            // set as default
+            button.style.opacity = '1';
+            button.removeAttribute("disabled");
+        }
+
+    });
+
+}
 
 function send_Token(e){
 	//stop default action
@@ -552,6 +629,8 @@ function savePost(e){
 // assigning events
 
 if(signup) signup.addEventListener('submit',register_user);
+
+if(resourceupload) resourceupload.addEventListener('submit',upload_resource);
 if(resetToken) resetToken.addEventListener('submit',send_Token);
 if(cameraBtn) imgDialog.addEventListener('change', updatePhoto);
 if(searchBtn) {
